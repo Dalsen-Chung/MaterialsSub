dc5cc3c4d1c884bb037f1c9687fe6e75675a4d63
Dalsen-Chung <codedalsen@163.com>
2017-08-25 17:45:16 +0800

    更新工作副本

--- laravel/resources/assets/js/components/Example.vue
+++ laravel/resources/assets/js/components/Example.vue
@@ -1,16 +1,8 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
-                    <div class="panel-body">
-                        I'm an example component!
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="jumbotron">
+        <h1>Hello, world!</h1>
+        <p>...</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
     </div>
 </template>
 
